a18d4a85b98385b51cf27dbe1613fb2ba6d2dc6f
Sean Yasnogrodski <sean.yasno@gmail.com>
2023-02-08 10:49:58 +0200

    Added firebase config into project.

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const config = {
+    firebase: {
+        apiKey: 'AIzaSyAUvU7O4s43JfslcHf1vHQCKVSNw2FZmvc',
+        authDomain: 'grocery-list-8c853.firebaseapp.com',
+        projectId: 'grocery-list-8c853',
+        storageBucket: 'grocery-list-8c853.appspot.com',
+        messagingSenderId: '847881902672',
+        appId: '1:847881902672:web:0232c4e2c0cb39d3f14f55',
+    },
+};
+
+export const app = initializeApp(config.firebase);
