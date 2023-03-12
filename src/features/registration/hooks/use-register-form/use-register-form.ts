f94af9674a53ceb0f97e8d3fe78ea3ba9ea405a2
Sean Yasnogrodski <sean.yasno@gmail.com>
2023-03-12 10:17:14 +0200

    Work on register form validation.

---
+++ src/features/registration/hooks/use-register-form/use-register-form.ts
@@ -0,0 +1,87 @@
+import { useFormik } from 'formik';
+import { isEmpty } from 'lodash';
+
+type RegisterInfo = {
+    fullName: string;
+    city: string;
+    street: string;
+    houseNumber: number;
+    entrance: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+};
+
+type RegisterInfoErrors = {
+    [key in keyof RegisterInfo]?: string;
+};
+
+export const useRegisterForm = () => {
+    return useFormik<RegisterInfo>({
+        initialValues: {
+            fullName: '',
+            city: '',
+            street: '',
+            houseNumber: 0,
+            entrance: '',
+            phoneNumber: '',
+            email: '',
+            password: '',
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        },
+        validate: (values) => {
+            const errors: Partial<RegisterInfoErrors> = {};
+            const {
+                fullName,
+                city,
+                email,
+                password,
+                houseNumber,
+                phoneNumber,
+                street,
+            } = values;
+
+            if (isEmpty(fullName)) {
+                errors.fullName = 'Required';
+            }
+
+            if (isEmpty(city)) {
+                errors.city = 'Required';
+            }
+
+            if (isEmpty(email)) {
+                errors.email = 'Required';
+            } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
+            ) {
+                errors.email = 'Invalid email address';
+            }
+
+            if (isEmpty(password)) {
+                errors.password = 'Required';
+            }
+
+            if (isEmpty(houseNumber)) {
+                errors.houseNumber = 'Required';
+            }
+
+            if (isEmpty(phoneNumber)) {
+                errors.phoneNumber = 'Required';
+            } else if (
+                !/^(?:(?:(\+?972|\(\+?972\)|\+?\(972\))(?:\s|\.|-)?([1-9]\d?))|(0[23489]{1})|(0[57]{1}[0-9]))(?:\s|\.|-)?([^0\D]{1}\d{2}(?:\s|\.|-)?\d{4})$/i.test(
+                    phoneNumber
+                )
+            ) {
+                errors.phoneNumber = 'Invalid phone number';
+            }
+
+            if (isEmpty(street)) {
+                errors.street = 'Required';
+            }
+
+            return errors;
+        },
+    });
+};
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { NextPage } from 'next';
 
 import { LoadingPage } from '@/components';
+import { cities } from '@/constants/cities';
 import {
     Background,
     BottomSection,
@@ -12,15 +13,18 @@ import {
     SubmitButton,
     Title,
 } from '@/features/registration';
+import { useRegisterForm } from '@/features/registration/hooks';
 import { useAuthValidation } from '@/hooks';
 import { theme } from '@/styles/theme';
-import { Grid, Stack } from '@mui/material';
+import { Autocomplete, Grid, Stack } from '@mui/material';
 
 const Register: NextPage = () => {
     const title = 'צור חשבון חדש';
     const registerButtonLabel = 'הירשם';
 
     const { loading, error } = useAuthValidation();
+    const { values, handleChange, setFieldValue, handleSubmit, errors } =
+        useRegisterForm();
 
     if (error) {
         return (
@@ -36,35 +40,203 @@ const Register: NextPage = () => {
 
     return (
         <Background>
-            <FormCard>
+            <form onSubmit={handleSubmit}>
+                <FormCard>
+                    <Stack
+                        display={{
+                            xs: 'none',
+                            md: 'flex',
+                        }}
+                        width={'50%'}
+                        margin={'auto 0'}
+                    >
+                        <Title>{title}</Title>
+                        <Grid container columnSpacing={2} rowSpacing={2}>
+                            <Grid item xs={6}>
+                                <FilledTextField
+                                    name={'fullName'}
+                                    value={values.fullName}
+                                    onChange={handleChange}
+                                    error={!!errors.fullName}
+                                    variant={'filled'}
+                                    label={'שם מלא'}
+                                    fullWidth
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Autocomplete
+                                    renderInput={(params) => (
+                                        <FilledTextField
+                                            ref={params.InputProps.ref}
+                                            inputProps={params.inputProps}
+                                            name={'city'}
+                                            value={values.city}
+                                            error={!!errors.city}
+                                            variant={'filled'}
+                                            label={'עיר מגורים'}
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true,
+                                            }}
+                                        />
+                                    )}
+                                    noOptionsText={'לא נמצאו ערים'}
+                                    options={cities.map((city) => city.name)}
+                                    onChange={(event, value) => {
+                                        setFieldValue('city', value);
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FilledTextField
+                                    name={'street'}
+                                    value={values.street}
+                                    onChange={handleChange}
+                                    error={!!errors.street}
+                                    variant={'filled'}
+                                    label={'רחוב'}
+                                    fullWidth
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FilledTextField
+                                    name={'houseNumber'}
+                                    value={values.houseNumber}
+                                    onChange={handleChange}
+                                    error={!!errors.houseNumber}
+                                    variant={'filled'}
+                                    label={'מספר בית'}
+                                    type={'number'}
+                                    fullWidth
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FilledTextField
+                                    name={'entrance'}
+                                    value={values.entrance}
+                                    onChange={handleChange}
+                                    error={!!errors.entrance}
+                                    variant={'filled'}
+                                    label={'כניסה'}
+                                    fullWidth
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FilledTextField
+                                    name={'phoneNumber'}
+                                    value={values.phoneNumber}
+                                    onChange={handleChange}
+                                    error={!!errors.phoneNumber}
+                                    variant={'filled'}
+                                    label={'טלפון'}
+                                    type={'tel'}
+                                    fullWidth
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FilledTextField
+                                    name={'email'}
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    error={!!errors.email}
+                                    variant={'filled'}
+                                    label={'אימייל'}
+                                    type={'email'}
+                                    fullWidth
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FilledTextField
+                                    name={'password'}
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    error={!!errors.password}
+                                    variant={'filled'}
+                                    label={'סיסמה'}
+                                    type={'password'}
+                                    fullWidth
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <SubmitButton
+                            type={'submit'}
+                            variant={'contained'}
+                            color={'secondary'}
+                        >
+                            {registerButtonLabel}
+                        </SubmitButton>
+
+                        <BottomSection
+                            topLabel={'או הירשם עם'}
+                            bottomLabel={'כבר יש לך חשבון?'}
+                            bottomLinkLabel={'התחבר'}
+                            href={'/login'}
+                        >
+                            <SocialButtons />
+                        </BottomSection>
+                    </Stack>
+                </FormCard>
+
                 <Stack
                     display={{
-                        xs: 'none',
-                        md: 'flex',
+                        xs: 'flex',
+                        md: 'none',
                     }}
-                    width={'50%'}
-                    margin={'auto 0'}
+                    width={'100%'}
+                    padding={'40px 20px'}
                 >
                     <Title>{title}</Title>
                     <Grid container columnSpacing={2} rowSpacing={2}>
-                        <Grid item xs={6}>
+                        <Grid item xs={12} md={6}>
                             <FilledTextField
+                                name={'fullName'}
+                                value={values.fullName}
+                                onChange={handleChange}
+                                error={!!errors.fullName}
                                 variant={'filled'}
                                 label={'שם מלא'}
                                 fullWidth
                                 InputProps={{ disableUnderline: true }}
                             />
                         </Grid>
-                        <Grid item xs={6}>
-                            <FilledTextField
-                                variant={'filled'}
-                                label={'עיר מגורים'}
-                                fullWidth
-                                InputProps={{ disableUnderline: true }}
+                        <Grid item xs={12} md={6}>
+                            <Autocomplete
+                                renderInput={(params) => (
+                                    <FilledTextField
+                                        name={'city'}
+                                        value={values.city}
+                                        onChange={handleChange}
+                                        error={!!errors.city}
+                                        ref={params.InputProps.ref}
+                                        inputProps={params.inputProps}
+                                        variant={'filled'}
+                                        label={'עיר מגורים'}
+                                        fullWidth
+                                        InputProps={{ disableUnderline: true }}
+                                    />
+                                )}
+                                noOptionsText={'לא נמצאו ערים'}
+                                options={cities.map((city) => city.name)}
+                                onChange={(event, value) => {
+                                    setFieldValue('city', value);
+                                }}
+                                value={values.city}
                             />
                         </Grid>
                         <Grid item xs={6}>
                             <FilledTextField
+                                name={'street'}
+                                value={values.street}
+                                onChange={handleChange}
+                                error={!!errors.street}
                                 variant={'filled'}
                                 label={'רחוב'}
                                 fullWidth
@@ -73,6 +245,10 @@ const Register: NextPage = () => {
                         </Grid>
                         <Grid item xs={6}>
                             <FilledTextField
+                                name={'houseNumber'}
+                                value={values.houseNumber}
+                                onChange={handleChange}
+                                error={!!errors.houseNumber}
                                 variant={'filled'}
                                 label={'מספר בית'}
                                 type={'number'}
@@ -80,16 +256,24 @@ const Register: NextPage = () => {
                                 InputProps={{ disableUnderline: true }}
                             />
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item xs={12} md={6}>
                             <FilledTextField
+                                name={'entrance'}
+                                value={values.entrance}
+                                onChange={handleChange}
+                                error={!!errors.entrance}
                                 variant={'filled'}
                                 label={'כניסה'}
                                 fullWidth
                                 InputProps={{ disableUnderline: true }}
                             />
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item xs={12} md={6}>
                             <FilledTextField
+                                name={'phoneNumber'}
+                                value={values.phoneNumber}
+                                onChange={handleChange}
+                                error={!!errors.phoneNumber}
                                 variant={'filled'}
                                 label={'טלפון'}
                                 type={'tel'}
@@ -97,8 +281,12 @@ const Register: NextPage = () => {
                                 InputProps={{ disableUnderline: true }}
                             />
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item xs={12} md={6}>
                             <FilledTextField
+                                name={'email'}
+                                value={values.email}
+                                onChange={handleChange}
+                                error={!!errors.email}
                                 variant={'filled'}
                                 label={'אימייל'}
                                 type={'email'}
@@ -106,8 +294,12 @@ const Register: NextPage = () => {
                                 InputProps={{ disableUnderline: true }}
                             />
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item xs={12} md={6}>
                             <FilledTextField
+                                name={'password'}
+                                value={values.password}
+                                onChange={handleChange}
+                                error={!!errors.password}
                                 variant={'filled'}
                                 label={'סיסמה'}
                                 type={'password'}
@@ -117,7 +309,11 @@ const Register: NextPage = () => {
                         </Grid>
                     </Grid>
 
-                    <SubmitButton variant={'contained'} color={'secondary'}>
+                    <SubmitButton
+                        type={'submit'}
+                        variant={'contained'}
+                        color={'primary'}
+                    >
                         {registerButtonLabel}
                     </SubmitButton>
 
@@ -126,107 +322,13 @@ const Register: NextPage = () => {
                         bottomLabel={'כבר יש לך חשבון?'}
                         bottomLinkLabel={'התחבר'}
                         href={'/login'}
+                        labelColor={'white'}
+                        bottomLinkColor={theme.palette.primary.main}
                     >
                         <SocialButtons />
                     </BottomSection>
                 </Stack>
-            </FormCard>
-
-            <Stack
-                display={{
-                    xs: 'flex',
-                    md: 'none',
-                }}
-                width={'100%'}
-                padding={'40px 20px'}
-            >
-                <Title>{title}</Title>
-                <Grid container columnSpacing={2} rowSpacing={2}>
-                    <Grid item xs={12} md={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'שם מלא'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'עיר מגורים'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'רחוב'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'מספר בית'}
-                            type={'number'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'כניסה'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'טלפון'}
-                            type={'tel'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'אימייל'}
-                            type={'email'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FilledTextField
-                            variant={'filled'}
-                            label={'סיסמה'}
-                            type={'password'}
-                            fullWidth
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                </Grid>
-
-                <SubmitButton variant={'contained'} color={'primary'}>
-                    {registerButtonLabel}
-                </SubmitButton>
-
-                <BottomSection
-                    topLabel={'או הירשם עם'}
-                    bottomLabel={'כבר יש לך חשבון?'}
-                    bottomLinkLabel={'התחבר'}
-                    href={'/login'}
-                    labelColor={'white'}
-                    bottomLinkColor={theme.palette.primary.main}
-                >
-                    <SocialButtons />
-                </BottomSection>
-            </Stack>
+            </form>
         </Background>
     );
 };
