d03ab649233329e060205ff7ae1cc88c4dfaa83b
Sean Yasnogrodski <sean.yasno@gmail.com>
2023-03-14 20:45:03 +0200

    Added eslint disable for title in document.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html lang="en" dir={'rtl'}>
             <Head>
+                {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
                 <title>אתר קניות</title>
                 <meta
                     name="description"
